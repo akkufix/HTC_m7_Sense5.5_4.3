d2b4172fe6b71082d83da7875f34762283b8c022
akkufix <xda@mkes.de>
2014-01-25 22:48:40 +0100

    first build eabi-4.6 (cleaned)

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jan 25 19:52:34 CET 2014"
+#define LINUX_COMPILE_BY "me"
+#define LINUX_COMPILE_HOST "amd1800"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
